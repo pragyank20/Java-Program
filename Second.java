5410d052e2d8f6853496674b2339887c558f9104
pragyank20 <90086045+pragyank20@users.noreply.github.com>
2023-03-14 11:42:37 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,9 @@
+class Second
+
+{  int a,b;
+   public static void main(String[]args)
+  {
+   System.out.println(args[0]);
+   System.out.print(args[1]);
+  }
+}
---
+++ third.java
@@ -0,0 +1,21 @@
+class Third
+
+{  int a,b;
+   public static void main(String[]args)
+  {
+    Third t1=new Third();
+    t1.get();
+    t1.put();
+  }
+
+
+void get(){
+a=3;
+b=4;
+}
+void put(){
+ System.out.println("Value of a:"+a);
+ System.out.println("Value of b:"+b);
+}
+}
+
