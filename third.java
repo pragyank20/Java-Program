ca9c5a4d46ac0663dbeb9ef04ed604b102aeb0b0
pragyank20 <90086045+pragyank20@users.noreply.github.com>
2023-03-14 11:50:32 +0530

    Update third.java

--- third.java
+++ third.java
@@ -3,15 +3,18 @@ class Third
 {  int a,b;
    public static void main(String[]args)
   {
+       int x=Integer.parseInt(args[0]);
+    int y=Integer.parseInt(args[1]);
     Third t1=new Third();
-    t1.get();
+    t1.get(x,y);
     t1.put();
+ 
   }
 
 
-void get(){
-a=3;
-b=4;
+void get(int x ,int y){
+a=x;
+b=y;
 }
 void put(){
  System.out.println("Value of a:"+a);
