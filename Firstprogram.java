e822d41f11f3222be7aebe9ecfbffd10f6b65932
pragyank20 <90086045+pragyank20@users.noreply.github.com>
2023-03-14 11:07:28 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,8 @@
+class Firstprogram
+
+{
+   public static void main(String[]args)
+  {
+    System.out.print("Hello Java");
+  }
+}
---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student
+
+{  int a,b;
+   public static void main(String[]args)
+  {
+   Student s1=new Student();
+   s1.a=4;
+   s1.b=5;
+   System.out.println("Value of a:"+s1.a);
+   System.out.print("Value of b:"+s1.b);
+  }
+}
